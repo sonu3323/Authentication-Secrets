12a916afe558ee7d04ca30fdf84fc6bea85d8c70
Sonu sharma <53683626+sonu3323@users.noreply.github.com>
2020-02-15 09:08:08 +0530

    Level-3 Hash Fuction

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -29,10 +29,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ['password'] });
-
-
-
 const User = new mongoose.model('user' , userSchema);
 
 
@@ -55,7 +51,7 @@ app.get('/login' , (req,res)=> {
 app.post("/register" ,(req,res) => {
     const newUser = new User({
         email : req.body.username ,
-        password : req.body.password
+        password : md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -72,8 +68,9 @@ app.post("/register" ,(req,res) => {
 
 app.post("/login" , (req,res)=> {
     const userName = req.body.username;
-    const password = req.body.password;
-
+    //Here we match the md5 fucntio and its match to our dataBase----
+    const password = md5(req.body.password);
+       
     User.findOne({email : userName} , (err,foundUser)=>{
         if(err) {
             console.log(err);
