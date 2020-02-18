d906954754e5347d1ba43b10d153f4b04a0a0321
Sonu sharma <53683626+sonu3323@users.noreply.github.com>
2020-02-18 15:24:07 +0530

    Level-5 Cookies and session

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require('passport');
+const passportLocalMongoose = require   ('passport-local-mongoose');
 
-
-const saltRound = 10;
 const app = express();
 
 //console.log(process.env.SECRET);
@@ -21,18 +21,38 @@ app.use(express.static('public'));
 //Use for ejs template ---
 app.set('view engine', 'ejs');
 
+//Cookies Session -----------------
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized:false 
+,
+}));
+
+//Passport initialized
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+
 //Create a connection to MongoDb Database-----
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
+mongoose.set("useCreateIndex" , true);
 //this is the Schema for user email and password-----
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('user', userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User . deserializeUser());
 
 
 app.get('/', (req, res) => {
@@ -49,23 +69,33 @@ app.get('/register', (req, res) => {
     res.render("register");
 });
 
+app.get("/secrets" ,(req,res)=>{
+   if(req.isAuthenticated) {
+       res.render("secrets");
+   }else {
+       res.redirect("/login");
+   }
+});
+
+app.get("/logout" , (req,res)=>{
+     req.logout();
+     res.redirect("/");
+});
+
 //Post method for the register router
 app.post("/register", (req, res) => {
-
-    bcrypt.hash(req.body.password, saltRound, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('secrets');
-            }
-        });
+    
+    User.register({username: req.body.username} , req.body.password ,(err,user)=>{
+        if(err) {
+            res.redirect("/register");
+        }else {
+           passport.authenticate("local")(req,res, (err,user)=>{
+            res.redirect("/secrets");
+           });
+          
+        }
     });
+  
 
 
 });
@@ -74,28 +104,22 @@ app.post("/register", (req, res) => {
 //post metnod for the login router-----
 
 app.post("/login", (req, res) => {
-    const userName = req.body.username;
-    //Here we match the md5 fucntio and its match to our dataBase----
-    const password = req.body.password;
 
-User.findOne({ email: userName }, (err, foundUser) => {
-if (err) {
-    console.log(err);
-} else {
-    if (foundUser) {
-       
-         bcrypt.compare(password, foundUser.password, (err, result) => {
-                if(result === true) {
-                    res.render('secrets');
-                }else {
-                    console.log(err);
-                }
-            });
-            
-        
-    }
-}
-})
+    const user =  new User({
+        username: req.body.username ,
+        password: req.body.password
+    });
+
+    req.login(user , (err)=>{
+        if(err) {
+            console.log(err);
+        }else {
+           passport.authenticate("local")(req,res ,()=>{
+               res.redirect("/secrets");
+           });
+        }
+    });
+
 });
 
 
